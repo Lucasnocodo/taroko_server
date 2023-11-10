ec17fb66b928aceed73bcc22a610da9900bad027
Lucas <h51301.c@gmail.com>
2023-11-10 22:19:34 +0800

    add view engine

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const specs = require('./docs/swaggerDocs');
 const app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // Open the server to public
 app.use(cors());
